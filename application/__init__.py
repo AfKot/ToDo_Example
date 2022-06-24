33def8a10e2218d2b35d4df74ce960bd0deeaa50
afzal <kotadia.afzal@gmail.com>
2022-06-24 13:53:37 +0000

    testing for index and about pages successful

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db" #os.getenv("DATABASE_URI")
+app.config['SECRET_KEY'] = "Shush"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
